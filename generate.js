40b4213a7f448474d34ead03667f184bfda77f62
Libikk <libikpl@gmail.com>
2021-01-27 07:52:27 +0000

    chore: update to puppeteer

--- generate.js
+++ generate.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 var fs = require("fs");
 const axios = require('axios');
 const nodeHtmlToImage = require('node-html-to-image')
@@ -20,21 +21,28 @@ const postImages = ({ redImage, blackImage }) => {
 
 }
 
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({ width: 880, height: 528 });
+    await page.goto('file://C:/Users/libik/Desktop/IT/repos/imagesGenerateFromHtml/template.html');
+    await page.evaluate(() => {
+      document.querySelector('body').classList.add('black')
+    });
+    await page.waitForSelector('.weatherCast');
 
-  nodeHtmlToImage({
-    html: htmlFile,
-    content: [
-      { isRed: false, output: './blackImage.png', lastUpdated: new Date().toLocaleString() },
-      { isRed: true, output: './redImage.png' }
-    ]
-  })
-  .then((e) => {
-    console.log('The image was created successfully!', e)
+    await page.screenshot({ path: './blackImage.png'})
+
+
+    await page.screenshot({ path: './redImage.png'})
+
+    await browser.close();
+
     const blackImage = fs.readFileSync('./blackImage.png');
     const base64BlackImage = new Buffer.from(blackImage).toString('base64');
 
     const redImage = fs.readFileSync('./redImage.png');
     const base64RedImage = new Buffer.from(redImage).toString('base64');
     return postImages({ redImage: base64RedImage, blackImage: base64BlackImage })
-  })
-  .catch(console.error)
+
+})();
