b3e37b1690746ee06f06f4e994ad73a90487063e
Libikk <libikpl@gmail.com>
2021-01-28 19:04:58 +0000

    chore: eslint config

--- generate.js
+++ generate.js
@@ -1,42 +1,39 @@
 const puppeteer = require('puppeteer');
-var fs = require("fs");
+const fs = require('fs');
 const axios = require('axios');
 
 const postImages = ({ redImage, blackImage }) => {
   const postData = {};
-  if (redImage) postData.redImage = `data:image/png;base64,${redImage}`
-  if (blackImage) postData.blackImage = `data:image/png;base64,${blackImage}`
+  if (redImage) postData.redImage = `data:image/png;base64,${redImage}`;
+  if (blackImage) postData.blackImage = `data:image/png;base64,${blackImage}`;
 
-  return axios.post('http://192.168.0.161:9999/imagesUpload',postData )
-  .then((res) => {
-    console.log('Update status:', res.data)
-  })
-  .catch(console.error);
-
-}
+  return axios.post('http://192.168.0.161:9999/imagesUpload', postData)
+    .then((res) => {
+      console.log('Update status:', res.data);
+    })
+    .catch(console.error);
+};
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setViewport({ width: 880, height: 528 });
-    await page.goto('file://C:/Users/libik/Desktop/IT/repos/imagesGenerateFromHtml/dist/index.html', {waitUntil: 'networkidle2'});
-    // await page.evaluate(() => {
-    //   document.querySelector('body').classList.add('black')
-    // });
-    // await page.waitForSelector('.KURWAMAC');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: 880, height: 528 });
+  await page.goto('file://C:/Users/libik/Desktop/IT/repos/imagesGenerateFromHtml/dist/index.html', { waitUntil: 'networkidle2' });
+  // await page.evaluate(() => {
+  //   document.querySelector('body').classList.add('black')
+  // });
+  // await page.waitForSelector('.KURWAMAC');
 
-    await page.screenshot({ path: './blackImage.png'})
+  await page.screenshot({ path: './blackImage.png' });
 
+  await page.screenshot({ path: './redImage.png' });
 
-    await page.screenshot({ path: './redImage.png'})
+  await browser.close();
 
-    await browser.close();
+  const blackImage = fs.readFileSync('./blackImage.png');
+  const base64BlackImage = new Buffer.from(blackImage).toString('base64');
 
-    const blackImage = fs.readFileSync('./blackImage.png');
-    const base64BlackImage = new Buffer.from(blackImage).toString('base64');
-
-    const redImage = fs.readFileSync('./redImage.png');
-    const base64RedImage = new Buffer.from(redImage).toString('base64');
-    return postImages({ redImage: base64RedImage, blackImage: base64BlackImage })
-
+  const redImage = fs.readFileSync('./redImage.png');
+  const base64RedImage = new Buffer.from(redImage).toString('base64');
+  return postImages({ redImage: base64RedImage, blackImage: base64BlackImage });
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,80 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const copyWebpackPlugin = require("copy-webpack-plugin");
-const Path = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const miniCssPlugin = new MiniCssExtractPlugin({
-  filename: "[name].[hash].css",
-  chunkFilename: "[id].[hash].css"
+  filename: '[name].[hash].css',
+  chunkFilename: '[id].[hash].css',
 });
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
+  template: './src/index.html',
+  filename: './index.html',
 });
 
-const copyPlugin = new copyWebpackPlugin([
+const copyPlugin = new CopyWebpackPlugin([
   {
-    from: "src/assets",
-    to: "dist/"
-  }
+    from: 'src/assets',
+    to: 'dist/',
+  },
 ]);
 
-module.exports = (env, argv) => {
-  return {
-    optimization: {
-      nodeEnv: argv.mode
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: "babel-loader"
-          }
+module.exports = (env, argv) => ({
+  optimization: {
+    nodeEnv: argv.mode,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-          test: /\.(sa|sc|c)ss$/,
-          use: [
-            argv.mode == "development"
-              ? "style-loader"
-              : MiniCssExtractPlugin.loader,
-            {
-              loader: "css-loader",
-              options: {
-                importLoaders: 1
-              }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          argv.mode == 'development'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
             },
-            "sass-loader"
-          ]
-        },
-        {
-          test: /\.(png|jpg|jp(e)g|gif|svg)$/i,
-          use: [
-            {
-              loader: "url-loader",
-              options: {
-                limit: 8192
-              }
-            }
-          ]
-        },
-        {
-          test: /\.(ttf|eot|svg|jpg|jp(e)g|png|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-          use: [
-            {
-              loader: "file-loader?name=assets[name].[ext]",
-              options: {
-                name(file) {
-                  if (argv.mode === "development") {
-                    return "[path][name].[ext]";
-                  }
-                  return "[hash].[ext]";
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|jp(e)g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|svg|jpg|jp(e)g|png|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: [
+          {
+            loader: 'file-loader?name=assets[name].[ext]',
+            options: {
+              name() {
+                if (argv.mode === 'development') {
+                  return '[path][name].[ext]';
                 }
-              }
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [htmlPlugin, miniCssPlugin, copyPlugin]
-  };
-};
+                return '[hash].[ext]';
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [htmlPlugin, miniCssPlugin, copyPlugin],
+});
