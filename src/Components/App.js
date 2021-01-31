6e0c69201f0c5327c9ca66e35def4009779bf162
Libikk <libikpl@gmail.com>
2021-01-31 19:45:09 +0000

    chore: tfl status

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 import Forecast from './Forecast';
+import TflStatus from './tflStatus';
 
 const DemoComponent = () => (
   <div className="rootContainer">
     <Forecast />
+    <TflStatus />
   </div>
 );
 
---
+++ src/Components/tflStatus.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import '../styles/tflStatus.scss';
+import axios from 'axios';
+
+const TflStatus = () => {
+  const [tflData, setTflData] = useState(null);
+
+  const getTflStatus = () => axios.get('https://api.tfl.gov.uk/line/mode/tube/status').then((r) => setTflData(r.data));
+
+  useEffect(() => {
+    getTflStatus();
+  }, []);
+
+  if (!tflData) return <div />;
+
+  const statuses = [
+    { key: 'jubilee' },
+    { key: 'metropolitan' },
+  ];
+
+  return (
+    <div className="tflstatuses">
+      {
+          statuses.map((singleStatus) => {
+            const lineData = tflData.find(({ id }) => id === singleStatus.key);
+            const statusDescription = lineData.lineStatuses[0].statusSeverityDescription;
+            return (
+              <div className="tflstatuses__status" key={singleStatus.key}>
+                <div className="status__name">{lineData.name}</div>
+                <div className="status__description">{statusDescription}</div>
+              </div>
+            );
+          })
+      }
+    </div>
+  );
+};
+
+export default TflStatus;
