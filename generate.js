da70272eef5e42b7e19e5658c809dc7e56df1f0a
Libikk <libikpl@gmail.com>
2022-08-16 17:36:05 +0100

    refactor

--- generate.js
+++ generate.js
@@ -1,15 +1,17 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const axios = require('axios');
-
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const Jimp = require("jimp");
+const axios = require("axios");
+const path = "../e-Paper/RaspberryPi_JetsonNano/python/myImages";
 const postImages = ({ redImage, blackImage }) => {
   const postData = {};
   if (redImage) postData.redImage = `data:image/png;base64,${redImage}`;
   if (blackImage) postData.blackImage = `data:image/png;base64,${blackImage}`;
-  console.log('Start uploading');
-  return axios.post('http://192.168.0.161:9999/imagesUpload', postData)
+  console.log("Start uploading");
+  return axios
+    .post("http://192.168.0.161:9999/imagesUpload", postData)
     .then((res) => {
-      console.log('Update status:', res.data);
+      console.log("Update status:", res.data);
     })
     .catch(console.error);
 };
@@ -18,31 +20,39 @@ const postImages = ({ redImage, blackImage }) => {
   // installation
   // https://samiprogramming.medium.com/puppeteer-on-raspbian-nodejs-3425ccea470e
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: '/usr/bin/chromium-browser',
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    executablePath: "/usr/bin/chromium-browser",
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 880, height: 528 });
-  await page.goto(`file://${__dirname}/dist/index.html`, { waitUntil: 'networkidle2' });
+  await page.goto(`file://${__dirname}/build/index.html`, {
+    waitUntil: "networkidle2",
+  });
   // await page.goto('http://localhost:8080', { waitUntil: 'networkidle2' });
   await page.evaluate(async () => {
-    document.querySelector('.rootContainer').classList.add('red');
-    document.querySelector('.rootContainer').classList.remove('black');
+    document.querySelector(".rootContainer").classList.add("red");
+    document.querySelector(".rootContainer").classList.remove("black");
   });
-  await page.screenshot({ path: './redImage.png' });
+  await page.screenshot({ path: `./redImage.png` });
 
   await page.evaluate(async () => {
-    document.querySelector('.rootContainer').classList.remove('red');
-    document.querySelector('.rootContainer').classList.add('black');
+    document.querySelector(".rootContainer").classList.remove("red");
+    document.querySelector(".rootContainer").classList.add("black");
   });
-  await page.screenshot({ path: './blackImage.png' });
+  await page.screenshot({ path: `./blackImage.png` });
 
   await browser.close();
 
-  const blackImage = fs.readFileSync('./blackImage.png');
-  const base64BlackImage = new Buffer.from(blackImage).toString('base64');
+  await Jimp.read("./blackImage.png", (err, lenna) => {
+    if (err) throw err;
+    console.log("Black image generated");
+    lenna.write(`${path}/blackImage.bmp`); // save
+  });
+  await Jimp.read("./redImage.png", (err, lenna) => {
+    if (err) throw err;
+    console.log("Red image generated");
+    lenna.write(`${path}/redImage.bmp`); // save
+  });
 
-  const redImage = fs.readFileSync('./redImage.png');
-  const base64RedImage = new Buffer.from(redImage).toString('base64');
-  return postImages({ redImage: base64RedImage, blackImage: base64BlackImage });
+  // return postImages({ redImage: base64RedImage, blackImage: base64BlackImage });
 })();
