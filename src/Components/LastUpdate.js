bbf87a65a15e884c9e10d4ccacfc39e61ca264c7
Marek Kregiel <marek@elmstonesystems.co.uk>
2022-08-28 11:18:35 +0100

    some improvments

--- src/Components/LastUpdate.js
+++ src/Components/LastUpdate.js
@@ -1,11 +1,11 @@
+import { format } from 'date-fns';
 import React from 'react';
 import '../styles/lastUpdate.scss';
 
 const LastUpdate = () => (
   <div className="last-update">
-    <img src="https://cdn.iconscout.com/icon/free/png-256/update-autorenew-refresh-reload-31805.png" alt="Update Icon" width="35" />
     <span>
-      {new Date().toLocaleString()}
+      {format(new Date(), 'kk:mm:ss MM/dd/yyyy')}
     </span>
   </div>
 );
